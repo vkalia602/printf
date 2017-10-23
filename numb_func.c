82d365651727a13c9c468f0381b2630715754758
vkalia602 <267@holbertonschool.com>
2017-10-23 22:42:59 +0000

    added print number function

--- numb_func.c
+++ numb_func.c
@@ -2,7 +2,30 @@
 #include <stdarg.h>
 void print_int(va_list ap)
 {
-	int c;
+	int c, i, output, reverse = 0, depth = 0;
 	c = va_arg(ap, int);
-	_putchar(c + '0');
+
+	if (c < 0)
+	{
+		_putchar('-');
+		c *= -1;
+	}
+
+	while (c != 0)
+	{
+		reverse *= 10;
+		reverse += (c % 10);
+		c /= 10;
+		depth++;
+	}
+
+	depth = depth == 0 ? 1 : depth;
+
+	for (i = 0; i < depth; i++)
+	{
+		output = reverse % 10;
+		_putchar(output + '0');
+		reverse /= 10;
+	}
+
 }
